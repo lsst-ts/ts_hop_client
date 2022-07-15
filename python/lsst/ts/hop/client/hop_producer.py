df6f1741d29e250902a6dcfd685f32ddf6c58160
bfc5288 <bfc5288@psu.edu>
2022-07-15 17:00:36 -0500

    Added autopep8 changes

--- python/lsst/ts/hop/client/hop_producer.py
+++ python/lsst/ts/hop/client/hop_producer.py
@@ -1,5 +1,6 @@
 import hop
 
+
 class hop_producer:
     """Produce kafka messages from SCiMMA Hopskotch kafka topics.
 
@@ -17,7 +18,7 @@ class hop_producer:
         self.hostname = hostname
         if auth == 'current':
             self.auth = hop.auth.select_matching_auth(hop.auth.load_auth(), hostname)
-        
+
     def _read_topic(self, topic='sys.heartbeat',
                     start_offset=hop.io.consumer.ConsumerStartPosition.LATEST):
         """Start streaming messages from a kafka topic.
@@ -35,7 +36,7 @@ class hop_producer:
         with stream.open(f'kafka://{self.hostname}/{topic}', 'r') as s:
             for message in s:
                 self._message_parsing(message)
-                
+
     def _message_parsing(self, message):
         """Placeholder function for processing a message. Currently just print the message.
 
@@ -48,7 +49,7 @@ class hop_producer:
     def get_earliest_offset(self):
         """Shorthand function to access the earliest kafka offset. Useful when wishing to stream
         all messages in a topic (previous and new).
-        
+
         """
 
         return hop.io.consumer.ConsumerStartPosition.EARLIEST
@@ -56,9 +57,10 @@ class hop_producer:
     def get_latest_offset(self):
         """Shorthand function to access the latest kafka offset. Useful when wishing to stream
         only new messages sent to a topic.
-        
+
         """
         return hop.io.consumer.ConsumerStartPosition.LATEST
+
 
 if __name__ == "__main__":
     scimma_hostname = 'kafka.scimma.org'
