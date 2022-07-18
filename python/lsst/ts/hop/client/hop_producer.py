5827b62b10fdfbb49116c8960be4870f41c97581
bfc5288 <bfc5288@psu.edu>
2022-07-18 11:14:08 -0500

    Added flake8 lint fixes

--- python/lsst/ts/hop/client/hop_producer.py
+++ python/lsst/ts/hop/client/hop_producer.py
@@ -17,9 +17,10 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.import hop
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import hop
+
 
 class hopProducer:
     """Produce kafka messages from SCiMMA Hopskotch kafka topics.
