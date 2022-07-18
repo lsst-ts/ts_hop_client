1d2cf1a3a1bf7bd6e8369d12ff5704fbf27c98aa
bfc5288 <bfc5288@psu.edu>
2022-07-18 11:11:18 -0500

    Added missing import

--- python/lsst/ts/hop/client/hop_producer.py
+++ python/lsst/ts/hop/client/hop_producer.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.import hop
 
+import hop
 
 class hopProducer:
     """Produce kafka messages from SCiMMA Hopskotch kafka topics.
